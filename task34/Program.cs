36bb57f65daab05d4dd832db3ff42ab51619c124
Sergey <rogozhinis@mail.ru>
2023-01-28 18:43:42 +0500

    task34

--- task34/Program.cs
+++ task34/Program.cs
@@ -6,15 +6,15 @@
 int size =new Random().Next(0,10000);
 
 int[]array= new int[size];
-int A=0;
+int QuantityEven=0;
 for (int i = 0; i < array.Length; i++)
 {
     array[i]=new Random().Next(100,1000);
     
     if (array[i]%2 ==0)
     {
-        A++;
+        QuantityEven++;
     }      
  }
     
- Console.WriteLine(A);
+ Console.WriteLine(QuantityEven);
